a7f047e0e47f962e9000453659eb77809076c006
rikimario <mario_martinov@abv.bg>
2025-10-21 00:54:49 +0300

    display user image if there is one

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import getUser from "@/utils/supabase/user";
 import { logout } from "@/app/get-started/actions";
+import Image from "next/image";
 
 export default async function Navbar() {
   const user = await getUser();
@@ -14,7 +15,17 @@ export default async function Navbar() {
       </Link>
       <div className="space-x-4">
         {user ? (
-          <Button onClick={logout}>Logout</Button>
+          <div className="flex gap-2">
+            <Image
+              className="rounded-full"
+              alt="profile_picture"
+              src={user?.user_metadata.picture}
+              width={40}
+              height={40}
+            />
+
+            <Button onClick={logout}>Logout</Button>
+          </div>
         ) : (
           <Link href="/get-started">
             <Button className={cn("bg-indigo-500 hover:bg-indigo-600")}>
