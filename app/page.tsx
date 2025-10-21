f3c0fa1fa9189b832e0607f0c1fc4cfec6ddc990
rikimario <mario_martinov@abv.bg>
2025-10-21 15:09:57 +0300

    add Hero component to Home section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import getUser from "@/utils/supabase/user";
+import Hero from "@/components/Hero";
 
 export default async function Home() {
-  const user = await getUser();
-
   return (
     <main>
-      <h1>Hello {user?.user_metadata.full_name}!</h1>
+      <Hero />
     </main>
   );
 }
