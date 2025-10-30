37857e1c2777fbeb9b2a7aae5f50a6feed80969a
rikimario <mario_martinov@abv.bg>
2025-10-30 20:25:14 +0200

    add subscription plans component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import CalcForm from "@/components/CalcForm";
 import Hero from "@/components/Hero";
+import SubscriptionPlans from "@/components/SubscriptionPlans";
 
 export default async function Home() {
   return (
     <main>
       <Hero />
       <CalcForm />
+      <SubscriptionPlans />
     </main>
   );
 }
