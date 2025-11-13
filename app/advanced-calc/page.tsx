0ea1f2fb196b079e0afce76ed8f5e4cf7dc55442
rikimario <mario_martinov@abv.bg>
2025-11-13 20:46:11 +0200

    add advanced calc form and check if user is premium

--- app/advanced-calc/page.tsx
+++ app/advanced-calc/page.tsx
@@ -1,5 +1,13 @@
+import AdvancedCalcForm from "@/components/AdvancedCalcForm";
+import getUser from "@/utils/supabase/user";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function AdvancedCalc() {
-  return <div>AdvancedCalc</div>;
+export default async function AdvancedCalc() {
+  const user = await getUser();
+
+  if (!user?.is_premium) {
+    redirect("/");
+  }
+  return <AdvancedCalcForm user={user} />;
 }
