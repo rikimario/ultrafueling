ed186137abdbef49bdf0e718902b4e08547f2414
rikimario <mario_martinov@abv.bg>
2025-11-26 00:32:17 +0200

    change next config for avatar storage

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,26 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     domains: ["lh3.googleusercontent.com"],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "svzirwqbkbfofdxhblhg.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
     domains: ["lh3.googleusercontent.com"],
   },
 };
