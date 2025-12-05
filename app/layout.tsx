65be89d2ce89a3b53527bdd446f2fdee2fd1636c
rikimario <mario_martinov@abv.bg>
2025-12-05 20:06:55 +0200

    change the max w to less

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default async function RootLayout({
       >
         <UserProvider user={user}>
           <Navbar />
-          <div className="max-w-[1440px] mx-auto py-4">
+          <div className="max-w-[1320px] mx-auto py-4">
             {children}
             <Toaster position="top-right" />
           </div>
